befc31c5bcf2f8760595a15c851935e6ddf14d9d
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-07-19 23:32:48 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) n *= -1;
+  let cifr = 0;
+  let a = n;
+  while(a> 9)
+  {
+      cifr++;
+      a /= 10;
+  }
+  cifr++;
+  let del = 1;
+  for(let i = 0; i < cifr - 1; i++)
+  {
+      del = del * 10;
+  }
+  let delOsn = del;
+  let deln = del;
+  let arr = [];
+  for(let i = 0; i < cifr; i++)
+  {
+    let temp = n;
+    for(let j = 0; j < i; j++){
+      temp -= arr[j]*del;
+      del /=10;
+    }
+    temp /= delOsn;
+    delOsn /= 10;
+    temp = Math.trunc(temp);
+    arr[i] = temp;
+    del = deln;
+  }
+  let sum = 0;
+  for(let i = cifr - 1; i != -1; i--)
+  {
+    sum += arr[i] * deln;
+    deln /= 10;
+  }
+  return sum;
 }
